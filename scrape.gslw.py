ea96426eb7c2e9105681b50563b9a32edabf3b80
Nick Kibre <nkibre@microsoft.com>
2025-08-22 13:02:40 -0700

    get images, fix stuff

--- scrape.gslw.py
+++ scrape.gslw.py
@@ -7,32 +7,49 @@ urls = [baseurl]
 
 didbefore = {}
 
-while(len(urls)):
-  url = urls.pop(0)
-  print("get: "+url)
-  print("stacksize: "+str(len(urls)))
-  content = str(urllib.request.urlopen(url).read())
-  for nexturl in re.findall(r'href="([^"]+)',content):
-    if not re.match(r'^https?:',nexturl):
-      nexturl = re.sub(r'[^\/]+$','',url)+nexturl
-    nexturl = re.sub(r'#.*$','',nexturl)
-    while re.search(r'[^/]+/\.\./',nexturl):
-      nexturl = re.sub(r'[^/]+/\.\./','',nexturl)
-    if re.search(r'/\.\.',nexturl):
-      raise Exception("relpath: "+nexturl)
-    nexturl = re.sub(r'(\w)\/+(\w)/',r'\1/\2/',nexturl)
-    print("next: "+nexturl)
-    if not re.search(r'\.html?$',nexturl):
-      print("notapage")
-    elif re.search(r'[^\w:/\.-]',nexturl):
-      print("badchar")
-    elif len(nexturl) < len(baseurl) or nexturl[0:len(baseurl)] != baseurl:
-      print("skipexternal")
-    elif re.search(r'(\?|=|new|video|help|links|support|article|copyright|advert)',nexturl):
-      print("skipquery")
-    elif nexturl in didbefore:
-      print("didbefore")
-    else:
-      print("APPEND")
-      didbefore[nexturl] = True
-      urls.append(nexturl)
+def urlnorm(url):
+  url = re.sub(r'#.*$','',url)
+  while re.search(r'[^/]+/\.\./',url):
+    url = re.sub(r'[^/]+/\.\./','',url)
+  if re.search(r'/\.\.',url):
+    raise Exception("relpath: "+url)
+  url = re.sub(r'(\w)\/+(\w)/',r'\1/\2/',url)
+  return url
+
+def main():
+
+  while(len(urls)):
+    url = urls.pop(0)
+    print("get: "+url)
+    print("stacksize: "+str(len(urls)))
+    content = str(urllib.request.urlopen(url).read())
+    for nexturl in re.findall(r'href="([^"]+)',content):
+      if not re.match(r'^https?:',nexturl):
+        nexturl = re.sub(r'[^\/]+$','',url)+nexturl
+      nexturl = urlnorm(nexturl)
+      print("next: "+nexturl)
+      if not re.search(r'\.html?$',nexturl):
+        print("notapage")
+      elif re.search(r'[^\w:/\.-]',nexturl):
+        print("badchar")
+      elif len(nexturl) < len(baseurl) or nexturl[0:len(baseurl)] != baseurl:
+        print("skipexternal")
+      elif re.search(r'(\?|=|new|video|help|links|support|article|copyright|advert)',nexturl):
+        print("skipquery")
+      elif nexturl in didbefore:
+        print("didbefore")
+      else:
+        print("APPEND")
+        didbefore[nexturl] = True
+        urls.append(nexturl)
+    for img in re.findall(r'img [^>]*src="([^"]+)',content):
+      if not re.match(r'^https?:',img):
+        img = re.sub(r'[^\/]+$','',url)+img
+      img = urlnorm(img)
+      if img in didbefore:
+        print("didimgbefore: "+img)
+      else:
+        print("getimg: "+url+" -> "+img)
+        didbefore[img] = True
+
+main()
