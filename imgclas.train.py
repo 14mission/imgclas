6835730eece7824a7924a372d8349fe80e415595
Nick Kibre, PhD <nkibre@gmail.com>
2025-08-30 21:26:50 -0700

    add multilabel mode

--- imgclas.train.py
+++ imgclas.train.py
@@ -6,29 +6,56 @@ import sys, os, re, glob, shutil
 def valid_image_path(path):
   try:
     with PILImage.open(path) as img:
-      img.verify()  # quick integrity check
-      return True
+      img.load()
+    return True
   except Exception:
     print(f"invalid image path {path}")
     return False
 
+csvfn = None
+multilabel = False
+num_train_epochs = 1
 av = sys.argv
 av.pop(0)
-if len(av) != 1:
+ac = 0
+while ac < len(av):
+  if av[ac][0] != '-' and csvfn == None: csvfn = av[ac]
+  elif av[ac][0] != '-': raise Exception("extra nonflag: "+av[ac])
+  elif av[ac] == "-m": multilabel = True
+  elif ac+1 == len(av) or av[ac+1][0] == '-': raise Exception("novalfor: "+av[ac])
+  elif av[ac] == "-e": ac+=1; num_train_epochs = int(av[ac])
+  else: raise Exception("unkflag: "+av[ac])
+  ac += 1
+if csvfn == None:
   raise Exception("usage: imgclas.train.py imglist.csv")
-csvfn = av[0]
 
 print("load csv")
 dataset = load_dataset("csv", data_files=csvfn)["train"]
 print(dataset[0])
+
 print("verify images")
 dataset = dataset.filter(lambda x: valid_image_path(x["image"]))
+
 print("load images")
 dataset = dataset.cast_column("image", Image())
 print(dataset[0])
+
 print("cnv labels")
 labels = list(set(dataset["label"]))
-dataset = dataset.cast_column("label", ClassLabel(names=labels))
+if multilabel:
+  splitlabels = set()
+  for lbl_str in labels:
+    split_labels = lbl_str.split(",")
+    splitlabels.update(split_labels)
+  labels = list(splitlabels)
+  print("num uniq split labels: "+str(len(labels)))
+  def encode_multi_label(example):
+    multi_hot = [1 if lbl in example["label"].split(",") else 0 for lbl in labels]
+    example["label"] = multi_hot
+    return example
+  dataset = dataset.map(encode_multi_label)
+else:
+  dataset = dataset.cast_column("label", ClassLabel(names=labels))
 print(dataset[0])
 
 print("labelmaps")
@@ -38,13 +65,14 @@ for i, label in enumerate(labels):
     id2label[str(i)] = label
     print(f"{i} {label}")
 
-print("numperlabel in train set")
-numperlabel = {}
-for item in dataset:
-  lblstr = id2label[str(item["label"])]
-  if lblstr not in numperlabel:
-    numperlabel[lblstr] = 0
-  numperlabel[lblstr] += 1
+#print("numperlabel in train set")
+#numperlabel = {}
+#for item in dataset:
+#  lblstrs = [id2label[str(item["label"])]]
+#  for libstr in libstrs:
+#    if lblstr not in numperlabel:
+#      numperlabel[lblstr] = 0
+#    numperlabel[lblstr] += 1
 
 # load vit model
 # (we need it first for turning images into features)
@@ -77,10 +105,19 @@ print("set up eval")
 import evaluate
 accuracy = evaluate.load("accuracy")
 import numpy as np
+from sklearn.metrics import f1_score, precision_score, recall_score
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
     predictions = np.argmax(predictions, axis=1)
     return accuracy.compute(predictions=predictions, references=labels)
+def compute_metrics_multilabel(eval_pred):
+    logits, labels = eval_pred
+    preds = (logits > 0).astype(int)   # threshold at 0
+    return {
+        "f1": f1_score(labels, preds, average="micro"),
+        "precision": precision_score(labels, preds, average="micro"),
+        "recall": recall_score(labels, preds, average="micro"),
+    }
 
 print("really load model")
 from transformers import AutoModelForImageClassification, TrainingArguments, Trainer
@@ -89,6 +126,7 @@ model = AutoModelForImageClassification.from_pretrained(
     num_labels=len(labels),
     id2label=id2label,
     label2id=label2id,
+    problem_type=("multi_label_classification" if multilabel else "single_label_classification"),
 )
 
 print("datasplit")
@@ -112,14 +150,11 @@ training_args = TrainingArguments(
     per_device_train_batch_size=16,
     gradient_accumulation_steps=4,
     per_device_eval_batch_size=16,
-    #num_train_epochs=10,
-    #num_train_epochs=5,
-    num_train_epochs=3,
-    #num_train_epochs=1,
+    num_train_epochs=num_train_epochs,
     warmup_ratio=0.1,
     logging_steps=10,
     load_best_model_at_end=True,
-    metric_for_best_model="accuracy",
+    metric_for_best_model=("eval_f1" if multilabel else "accuracy"),
 )
 
 trainer = Trainer(
@@ -129,7 +164,7 @@ trainer = Trainer(
     train_dataset=dataset_train,
     eval_dataset=dataset_vali,
     processing_class=image_processor,
-    compute_metrics=compute_metrics,
+    compute_metrics=(compute_metrics_multilabel if multilabel else compute_metrics),
 )
 
 trainer.train()
