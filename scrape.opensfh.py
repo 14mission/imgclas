1be89101214bb31d213bf271a919f8c4154bcdbf
Nick Kibre, PhD <nkibre@gmail.com>
2025-09-03 20:47:43 -0700

    more excluded words

--- scrape.opensfh.py
+++ scrape.opensfh.py
@@ -103,7 +103,7 @@ for nw in [1,2,3]:
     if i >= 25:
       break
 
-excludewords = "the be to of and a in that have i it for not on with he as you do at this but his by from they we say her she or an will my one all would there their what so up out if about who get which go me when make can like time no just him know take people into year your good some could them see other than then now look only come its over think also back after use two how our work first well way even new want because any these give day most us".split()
+excludewords = "the be to of and a in that have i it for not on with he as you do at this but his by from they we say her she or an will my one all would there their what so up out if about who get which go me when make can like time no just him know take people into year your good some could them see other than then now look only come its over think also back after use two how our work first well way even new want because any these give day most us is are were where location".split()
 
 # sort data in an order that's sorta random but actually deterministic
 def fnsortfunc(fn):
